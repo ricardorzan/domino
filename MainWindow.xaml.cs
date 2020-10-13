9f65af009424b5744ac60fc4418e5c8c7acb08f5
ricardorzan <62387274+ricardorzan@users.noreply.github.com>
2020-10-13 13:56:36 -0500

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Domino
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private object content;
+        public MainWindow()
+        {
+            InitializeComponent();
+            content = Content;
+        }
+
+        private void registrarse_Click(object sender, RoutedEventArgs e)
+        {
+            Registrarse registrarse = new Registrarse(this);
+            this.Content = registrarse;
+        }
+
+        public void GoBackToStartPage()
+        {
+            Content = content;
+        }
+    }
+}
